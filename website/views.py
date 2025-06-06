b76a2749ab46549156b3da81c45ac2917298eeaa
C-A08 <alejandrojuan.carey@gmail.com>
2025-06-06 16:06:30 -0500

    Final Push

--- website/views.py
+++ website/views.py
@@ -11,3 +11,27 @@ def home():
 def second():
     return render_template("second.html")
 
+@viewss.route('/third')
+def third():
+    return render_template("third.html")
+
+@viewss.route('/Google_form')
+def Google_form():
+    return render_template("Google_form.html")
+@viewss.route('/Success')
+def Success():
+    return render_template("Success.html")
+@viewss.route('/spanishVersion')
+def spanishVersion():
+    return render_template("spanishVersion.html")
+@viewss.route('/SpanishGoogleForm')
+def SpanishGoogleForm():
+    return render_template("SpanishGoogleForm.html")
+@viewss.route('/SpanishSuccess')
+def SpanishSuccess():
+    return render_template("SpanishSuccess.html")
+
+@viewss.route('/Map')
+def Map():
+    return render_template("Map.html")
+
